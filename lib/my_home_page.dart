b7ca5bbf8d0592da77445d070330af2327a0f2be
NiyomugaboFidel <fidelniyomugabo67@gmail.com>
2026-01-03 23:10:39 +0200

    add tabbar

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -3,16 +3,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:healthapp/app_colors.dart' as AppColors;
+import 'package:healthapp/my_tabs.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage() : super();
+  const MyHomePage({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   List popularBooks = [];
+
+  late final ScrollController _scrollController;
+  late final TabController _tabController;
 
   void ReadData() async {
     await DefaultAssetBundle.of(
@@ -27,6 +32,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    _scrollController = ScrollController();
+    _tabController = TabController(length: 3, vsync: this);
     ReadData();
   }
 
@@ -106,6 +113,93 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                     ),
                   ],
+                ),
+              ),
+
+              Expanded(
+                child: NestedScrollView(
+                  controller: _scrollController,
+                  headerSliverBuilder: (BuildContext context, bool isScroll) {
+                    return [
+                      SliverAppBar(
+                        pinned: true,
+                        backgroundColor: AppColors.sliverBackground,
+                        bottom: PreferredSize(
+                          preferredSize: Size.fromHeight(50),
+
+                          child: Container(
+                            margin: const EdgeInsets.only(bottom: 20, left: 10),
+
+                            child: TabBar(
+                              indicatorPadding: const EdgeInsets.all(0),
+                              indicatorSize: TabBarIndicatorSize.label,
+                              labelPadding: const EdgeInsets.only(right: 10),
+                              controller: _tabController,
+                              isScrollable: true,
+                              indicator: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.2),
+                                    blurRadius: 7,
+                                    offset: Offset(0, 0),
+                                  ),
+                                ],
+                              ),
+                              tabs: [
+                                AppTabs(
+                                  color: AppColors.menu1Color,
+                                  text: "New",
+                                ),
+                                AppTabs(
+                                  color: AppColors.menu2Color,
+                                  text: "Popular",
+                                ),
+                                AppTabs(
+                                  color: AppColors.menu3Color,
+                                  text: "Trending",
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ];
+                  },
+
+                  body: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      ListView.builder(itemBuilder: (_,i){
+
+                        return Container(
+                            
+                            margin:const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+                            
+                       
+                        );
+
+                      }),
+                      Material(
+                        child: ListTile(
+                          leading: CircleAvatar(backgroundColor: Colors.grey),
+                          title: Text("Content"),
+                        ),
+                      ),
+                      Material(
+                        child: ListTile(
+                          leading: CircleAvatar(backgroundColor: Colors.grey),
+                          title: Text("Content"),
+                        ),
+                      ),
+                      Material(
+                        child: ListTile(
+                          leading: CircleAvatar(backgroundColor: Colors.grey),
+                          title: Text("Content"),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
